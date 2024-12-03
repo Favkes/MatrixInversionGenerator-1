9c4b7a7d5237ebe6a82d3fffccb48bce7b1c5216
Favkes <tho.strongest.avenger@gmail.com>
2024-12-03 20:19:51 +0100

    Implemented real arithmetic, allowing for infinite precision instead of relying on floating point numbers.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
     I have manually created a working equation matrix for finding
     inverses of 2x2 matrices, however even 3x3 have already proven
     too complex to be done by hand.
-    Therefore this code will generate these huge equations automatically,
+    Therefore, this code will generate these huge equations automatically,
     and potentially simplify them as well.
 """
 import numpy as np
@@ -23,18 +23,82 @@ def gen_elements_matrix(dims: tuple[int, int]):
     return arr
 
 
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+def lcm(a, b):
+    return (a*b) // gcd(a, b)
+
+
+class RealNumber:
+    numerator: int
+    denominator: int
+
+    def __init__(self, num: int = 0, den: int = 1):
+        self.numerator = num
+        self.denominator = den
+
+    def __mul__(self, other):
+        self.numerator *= other.numerator
+        self.denominator *= other.denominator
+        return self
+
+    def __truediv__(self, other):
+        other.numerator, other.denominator = other.denominator, other.numerator
+        return self * other
+
+    def __add__(self, other):
+        num1 = self.numerator
+        den1 = self.denominator
+        num2 = other.numerator
+        den2 = other.denominator
+
+        lcd = lcm(den1, den2)
+        num1 *= lcd // den1
+        num2 *= lcd // den2
+
+        self.numerator = num1+num2
+        self.denominator = lcd
+        return self
+
+    def __neg__(self):
+        self.numerator = -self.numerator
+        return self
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+
 class ArithmeticalMatrix:
+    dims: tuple[int, int]
     unit_matrix: np.array
     body_matrix: np.array
     input_matrix: np.array
 
     def __init__(self, input_matrix: np.array):
+        self.dims = input_matrix.shape[:2]
         self.input_matrix = input_matrix
         self.body_matrix = gen_elements_matrix(input_matrix.shape[:2])
         self.unit_matrix = np.diag([1,] * input_matrix.shape[0])
 
+    def alpha(self):
+        """Gaussian elimination part of the algorithm."""
+        y, x = self.dims
+        A = self.input_matrix
+        for y_ in range(1, y):
+            psi = A[y_, 0] / A[0, 0]
+            for x_ in range(x):
+                A[y_, x_] = A[y_, x_] - A[0, x_] * psi
+
+
     def __str__(self):
-        y, x = self.input_matrix.shape
+        y, x = self.dims
         inM = self.input_matrix
         bdM = self.body_matrix
         unM = self.unit_matrix
@@ -49,8 +113,12 @@ class ArithmeticalMatrix:
         return s
 
 
-A = np.round(np.random.rand(2, 2) * 10)
+A = np.round(np.random.rand(3, 3) * 10)
 I = np.diag([1,] * 2)
 
 M = ArithmeticalMatrix(A)
-print(M)
+# print(M)
+
+M.alpha()
+# print(M)
+
